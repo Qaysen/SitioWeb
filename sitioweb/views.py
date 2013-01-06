2f072fefe2a1e5d1b3966dd65a640e82628211b0
Edwin Gonzales Melquiades <edwinpgm@outlook.com>
2013-01-06 00:05:39 -0500

    Commit Inicial

---
+++ sitioweb/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+def inicio(request):
+	return render_to_response('inicio.html', context_instance=RequestContext(request))
