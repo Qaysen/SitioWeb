196ebfece623327f6c161e83a1b9c69fbe68d5cb
Michael <michael@ubuntu.ubuntu-domain>
2013-01-10 15:56:59 -0500

    hola

--- sitioweb/views.py
+++ sitioweb/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 #probando nuevos cambios.
+""""Hola """
 def inicio(request):
 	return render_to_response('inicio.html', context_instance=RequestContext(request))
